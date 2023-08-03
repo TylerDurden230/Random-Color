14f671cc7d0c98e2a2eccbfd3dca03c7e706d549
Flavio <flavio.dagnese@gmail.com>
2023-08-03 11:05:31 +0200

    Colors are saved in localStorage, ColorContainer is now more responsive and updates his height basen on window innerWidth and capacity

--- script.js
+++ script.js
@@ -1,97 +1,131 @@
-let body = document.getElementById('body');
-let colorTxt = document.getElementById('color');
-let copyBtn = document.getElementById('copy');
-let saveBtn = document.getElementById('save');
-let saved = document.getElementById('saved');
+let body = document.getElementById("body");
+let colorTxt = document.getElementById("color");
+let copyBtn = document.getElementById("copy");
+let saveBtn = document.getElementById("save");
+let saved = document.getElementById("saved");
 let capacity;
 let wWidth = window.innerWidth;
 const ratio = 200;
 let s = false;
 let savedColors = [];
-let newColor = '';
-
+let newColor = "";
 let closeBtns;
 
-function generateRandom(maxLimit){
-	let rand = Math.random() * maxLimit;
-	rand = Math.floor(rand);
-	return rand;
+window.addEventListener("resize", () => {
+  wWidth = window.innerWidth;
+  calcCapacity();
+  let savedContainerHeight = Math.ceil(savedColors.length / capacity) * 175;
+  saved.style.height = savedContainerHeight + "px";
+  console.log("savedHeight",savedContainerHeight, "Capacity: ",capacity, savedColors.length, wWidth);
+  showColors();
+});
+
+function init() {
+  if (localStorage.getItem("savedColors") != null)
+    savedColors = JSON.parse(localStorage.getItem("savedColors"));
+  calcCapacity();
+  showColors();
 }
 
-function calcCapacity(){
-	capacity = Math.floor(wWidth / ratio);
-	console.log(wWidth);
-	console.log(capacity);
+function generateRandom(maxLimit) {
+  let rand = Math.random() * maxLimit;
+  rand = Math.floor(rand);
+  return rand;
 }
 
-function generateHexa(){
-	const arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-	let color = '#';
-	for(let i = 0; i < 6; i++)
-		color += arr[generateRandom(16)];
-	console.log(color);
-	return color;
+function calcCapacity() {
+  capacity = Math.floor(wWidth / ratio);
+  console.log(wWidth);
+  console.log(capacity);
+}
+
+function generateHexa() {
+  const arr = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  let color = "#";
+  for (let i = 0; i < 6; i++) color += arr[generateRandom(16)];
+  console.log(color);
+  return color;
 }
 
 function changeColor() {
-	copyBtn.innerHTML = 'Copy';
-	saveBtn.innerHTML = 'Save';
-	s = false;
-	newColor = generateHexa();
-	body.style.backgroundColor = newColor;
-	colorTxt.innerHTML = newColor;
-	copyBtn.style.display = 'block';
+  copyBtn.innerHTML = "Copy";
+  saveBtn.innerHTML = "Save";
+  s = false;
+  newColor = generateHexa();
+  body.style.backgroundColor = newColor;
+  colorTxt.innerHTML = newColor;
+  copyBtn.style.display = "block";
 }
 
-function clear(elementToClear)
-{
-	elementToClear.innerHTML = '';
+function clear(elementToClear) {
+  elementToClear.innerHTML = "";
 }
 
 function saveColor() {
-	if (!s)
-		if(savedColors.length < capacity) {
-			savedColors.push(newColor);
-			saveBtn.innerHTML = 'Saved!';
-			s = true;
-			showColors();
-		}
-		else 
-		{
-			savedColors.shift();
-			savedColors[capacity - 1] = newColor;
-			s = true;
-			showColors();
-		}
-	else 
-		saveBtn.innerHTML = 'Already saved!';
+  if (!s)
+    if (savedColors.length < capacity) {
+      savedColors.push(newColor);
+      saveBtn.innerHTML = "Saved!";
+      s = true;
+      showColors();
+    } else {
+      savedColors.shift();
+      savedColors[capacity - 1] = newColor;
+      s = true;
+      showColors();
+    }
+  else saveBtn.innerHTML = "Already saved!";
+  localStorage.setItem("savedColors", JSON.stringify(savedColors));
 }
 
 function removeColor(id) {
-	const index = savedColors.indexOf(id);
-	if (index > -1)
-		savedColors.splice(index, 1); // 2nd parameter means remove one item only
-	showColors();
+  const index = savedColors.indexOf(id);
+  if (index > -1) savedColors.splice(index, 1); // 2nd parameter means remove one item only
+  showColors();
+  localStorage.setItem("savedColors", JSON.stringify(savedColors));
 }
 
 function showColors() {
-		clear(saved);
-		for(let i = 0; i < savedColors.length; i++) {
-			saved.innerHTML += `<div id="${savedColors[i]}" class="box" style="background-color: ${savedColors[i]};">
+  clear(saved);
+  for (let i = 0; i < savedColors.length; i++) {
+    saved.innerHTML += `<div id="${savedColors[i]}" class="box" style="background-color: ${savedColors[i]};">
 			<button onclick="copy('${savedColors[i]}')" class="copy">c</button>
 			<button onclick="removeColor('${savedColors[i]}')" class="close">x</button>
 			<h6>${savedColors[i]}</h6></div>`;
-	}
+  }
 }
+
+// resize saved container height based on window width and capacit. this should listen to the event window.onresize
+function resizeSavedContainer() {
+
+}
+
+
 
 function copy(id) {
-	navigator.clipboard.writeText(id);
-	alert("Copied");
+  navigator.clipboard.writeText(id);
+  alert("Copied");
 }
 
-function copyText() {  
-	let copyTxt = document.getElementById('color').innerHTML;
-	navigator.clipboard.writeText(copyTxt);
-	copyBtn.innerHTML = 'Copied!'
-  }
-  
+function copyText() {
+  let copyTxt = document.getElementById("color").innerHTML;
+  navigator.clipboard.writeText(copyTxt);
+  copyBtn.innerHTML = "Copied!";
+}
